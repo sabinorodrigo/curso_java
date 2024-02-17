3d0aa5d398c47d4963aaba9560350158027c6b7e
sabinorodrigo <sr.rodrigosabino@gmail.com>
2024-02-17 11:20:00 -0300

    Hello World!

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -2,4 +2,9 @@ package fundamentos;
 
 public class PrimeiroPrograma {
 
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		// Imprima uma linha na saída so sistema
+	} 
 }
