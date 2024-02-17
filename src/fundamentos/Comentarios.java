99a47b449ad51f5a05f87575820493d6d2693010
sabinorodrigo <sr.rodrigosabino@gmail.com>
2024-02-17 12:25:58 -0300

    comentarios em Java

---
+++ src/fundamentos/Comentarios.java
@@ -0,0 +1,32 @@
+package fundamentos;
+
+public class Comentarios {
+
+	/**
+	 * Essa classe representa...
+	 * 
+	 * @author Rodrigo Sabino Gomes
+	 */
+	public static void main(String[] args) {
+		
+		// comentario de uma linha
+		// Uma sentença de código termina com ;
+		System.out.println("Comentários em Java!");
+		
+		/*
+		 *comentarios de multiplas linhas
+		 *Linha 1
+		 *Linha 2
+		 *Linha 3
+		 */
+		
+		System.out.println("Fim");// Aqui tb funciona!
+		
+		/*
+		 * Comentar um código em ambiente profissional é não algo muito legal.
+		 * não é indicado fazer a menos que a empresa em que você trabalha exija isso.
+		 * Mas no ambiente de aprendizado é altamente indicado fazer comentarios pois irá ajuda-lo 
+		 * a memorizar o que se está aprendendo.
+		 */
+	}
+}
--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -3,8 +3,14 @@ package fundamentos;
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-		
+
+		System.out.println("Hello World! part #01");
+		System.out.println("Hello World! part #02");
 		System.out.println("Hello World!");
 		// Imprima uma linha na saída so sistema
-	} 
+
+		// Java é uma sentença de código e uma sentença termina em ponto e vígula (;)
+		// E há os blocos de códigos que ficam entre chaves ({})
+		// E um bloco de sempre fica entre outro bloco
+	}
 }
