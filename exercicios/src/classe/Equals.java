47bc891aa94fba679c18a08c715895899977fb98
Rodrigo Sabino <sr.rodrigosabino@gmail.com>
2025-03-27 11:53:28 -0300

    mais sobre arrays

---
+++ exercicios/src/classe/Equals.java
@@ -0,0 +1,24 @@
+package classe;
+
+import java.util.Date;
+
+public class Equals {
+
+	public static void main(String[] args) {
+		
+		Usuario u1 = new Usuario();
+		u1.nome = "Bilbo Bolseiro";
+		u1.email = "bilbo.bolseiro@email.com";
+		
+		Usuario u2 = new Usuario();
+		u2.nome = "Bilbo Bolseiro";
+		u2.email = "bilbo.bolseiro@email.com";
+		
+		System.out.println(u1 == u2);
+		System.out.println(u1.equals(u2));
+		System.out.println(u2.equals(u1));
+		
+		System.out.println(u2.equals(new Date()));
+		
+	}
+}
---
+++ exercicios/src/classe/Usuario.java
@@ -0,0 +1,29 @@
+package classe;
+
+public class Usuario {
+
+	String nome;
+	String email;
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object objeto) {
+		
+		if (objeto instanceof Usuario) {
+			Usuario outro = (Usuario) objeto;
+			
+			boolean nomeIgual = outro.nome.equals(this.nome);
+			boolean emailIgual = outro.email.equals(this.email);
+			
+			return nomeIgual && emailIgual;
+		} else {
+			return false;
+		}
+	}
+	
+	
+}
