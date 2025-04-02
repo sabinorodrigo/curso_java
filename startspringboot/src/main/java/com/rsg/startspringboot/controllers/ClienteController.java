0d205050b38c99c911594052ac0aa1f76ba6e1b6
Rodrigo Sabino <sr.rodrigosabino@gmail.com>
2025-04-02 11:04:18 -0300

    Updated files in .gitignore and startspringboot

--- startspringboot/src/main/java/com/rsg/startspringboot/controllers/ClienteController.java
+++ startspringboot/src/main/java/com/rsg/startspringboot/controllers/ClienteController.java
@@ -1,7 +1,9 @@
 package com.rsg.startspringboot.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rsg.startspringboot.models.Cliente;
@@ -13,5 +15,15 @@ public class ClienteController {
 	@GetMapping("/qualquer")
 	public Cliente obterCliente() {
 		return new Cliente(28, "Bilbo", "123.456.789-00");
+	}
+	
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Dark Magician", "322.356.553-34");
+	}
+	
+	@GetMapping
+	public Cliente obterClientePorId2(@RequestParam(name = "id", defaultValue = "1") int id) {
+		return new Cliente(id, "Celtic Guardian", "233.892.908-38");
 	}
 }
