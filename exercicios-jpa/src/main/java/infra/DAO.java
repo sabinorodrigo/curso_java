5c1b392b4c9ea58d27e2ba93325ad51cdd8781c6
Rodrigo Sabino Gomes <sr.rodrigosabino@gmail.com>
2025-05-21 19:06:39 -0300

    criado entidade produto, mapeando a entidade produto, criado Data Acess Object (DAO)

---
+++ exercicios-jpa/src/main/java/infra/DAO.java
@@ -0,0 +1,73 @@
+package infra;
+
+import java.util.List;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+public class DAO<E> {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	Class<E> classe;
+	
+	static {
+		try {
+			emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+		} catch (Exception e) {
+			// 
+		}
+	}
+	
+	public DAO() {
+		this(null);
+	}
+	
+	public DAO(Class<E> classe) {
+		this.classe = classe;
+		em = emf.createEntityManager();
+	}
+	
+	public DAO<E> abrirT() {
+		em.getTransaction().begin();
+		return this;
+	}
+	
+	public DAO<E> fecharT() {
+		em.getTransaction().commit();
+		return this;
+	}
+	
+	public DAO<E> incluir(E entidade) {
+		em.persist(entidade);
+		return this;
+	}
+	
+	public DAO<E> incluirAtomico(E entidade) {
+		
+		return this.abrirT().incluir(entidade).fecharT();
+	}
+	
+	public List<E> obterTodos() {
+		return this.obterTodos(10, 0);
+	}
+	
+	public List<E> obterTodos(int qtde, int deslocamento) {
+		if (classe == null) {
+			throw new UnsupportedOperationException("Classe nula.");
+		}
+		
+		String jpql = "select e from " + classe.getName() + " e";
+		TypedQuery<E> query = em.createQuery(jpql, classe);
+		query.setMaxResults(qtde);
+		query.setFirstResult(deslocamento);
+		return query.getResultList();
+	}
+	
+	public void fechar() {
+		em.close();
+	}
+}
+
--- exercicios-jpa/src/main/java/teste/basico/RemoverUsuario.java
+++ exercicios-jpa/src/main/java/teste/basico/RemoverUsuario.java
@@ -3,6 +3,7 @@ package teste.basico;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import model.basico.Usuario;
 
 public class RemoverUsuario {
 
@@ -10,6 +11,14 @@ public class RemoverUsuario {
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
 		EntityManager em = emf.createEntityManager();
+		
+		Usuario usuario = em.find(Usuario.class, 16L);
+		
+		if (usuario != null) {
+			em.getTransaction().begin();
+			em.remove(usuario);
+			em.getTransaction().commit();
+		}
 		
 		em.close();
 		emf.close();
